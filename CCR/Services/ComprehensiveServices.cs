8011e69dc4ff9220b157920795f8e70b6439d026
DELL <DELL@USER>
2024-06-15 23:04:28 +0100

    Add project files.

---
+++ CCR/Services/ComprehensiveServices.cs
@@ -0,0 +1,28 @@
+﻿using CCR.Models;
+using IronXL;
+
+namespace CCR.Services
+{
+    public class ComprehensiveServices : IComprehensiveServices
+    {
+        public IEnumerable<ComprehensiveView> ExtractComprehensive(IFormFile file)
+        {
+            WorkBook workBook = WorkBook.Load(file.OpenReadStream());
+            WorkSheet workSheet = workBook.DefaultWorkSheet;
+            var comps = new List<ComprehensiveView>();
+            for (int i = 12; i < workSheet.RowCount; i++)
+            {
+                Cell cellB = workSheet[$"B{i}"].First();
+                Cell cellN = workSheet[$"N{i}"].First();
+                if(string.IsNullOrEmpty(cellB.StringValue))
+                    break;
+                comps.Add(new ComprehensiveView
+                {
+                    SettlementNarration = cellB.StringValue,
+                    Amount = cellN.DecimalValue,
+                });
+            }
+            return comps;
+        }
+    }
+}
